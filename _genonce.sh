7f6527bc28b01ff6a4cbfa3a1475ae67f93492c1
Chris Moesel <cmoesel@mitre.org>
2023-05-26 18:47:51 -0400

    Document invariant rules

--- _genonce.sh
+++ _genonce.sh
@@ -14,6 +14,8 @@ fi
 
 echo "$txoption"
 
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
+
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
 	java -jar $publisher -ig . $txoption $*
